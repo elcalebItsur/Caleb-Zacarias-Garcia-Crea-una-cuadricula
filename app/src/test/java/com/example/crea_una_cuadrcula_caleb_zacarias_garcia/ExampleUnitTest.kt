fb2edadf36529d33e2b172bc9c62c4ef732172d2
elcalebItsur <s22120212@alumnos.itsur.edu.mx>
2025-09-24 23:21:50 -0600

    Commit inicial del proyecto Cuadrícula - Caleb Zacarias Garcia

---
+++ app/src/test/java/com/example/crea_una_cuadrcula_caleb_zacarias_garcia/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.crea_una_cuadrcula_caleb_zacarias_garcia
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
